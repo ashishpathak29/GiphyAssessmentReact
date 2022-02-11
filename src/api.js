2e8e3a42403fd3f8f0edd2f945f033a578c9e592
AshishPathak2910 <ashishpathak29@users.noreply.github.com>
2022-02-11 21:27:35 +0530

    new changes

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
-const {REACT_APP_API_KEY} = process.env
+
 
 const BASE_URL = 'https://api.giphy.com/v1/gifs'
 const SEARCH_URL = `${BASE_URL}/search`
 const TRENDING_URL = `${BASE_URL}/trending`
+const REACT_APP_API_KEY = 'tVaJe9QRTL6VZp9xhBkogbNWFTI9hYnJ'
 
 
 export async function getTrendingItem () {
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 export default function SearchBar ({onSubmit}) {
     const [query, setQuery] = useState('')
